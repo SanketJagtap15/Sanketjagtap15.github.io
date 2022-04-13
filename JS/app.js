b9bd32062ec0dc219d51b6f1d0121b8b408370a7
SanketJagtap15 <sanket15@kgpian.iitkgp.ac.in>
2022-04-13 13:38:29 +0530

    My commit

---
+++ JS/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+    
+    
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+    
+    hamberger.addEventListener('click', function(){
+      mobileNav.classList.add('open');  
+    });
+    
+    times.addEventListener('click', function(){
+        mobileNav.classList.remove('open');  
+    });
+    
+    });
